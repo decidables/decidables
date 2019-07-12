54af46d741189a2b05c4217a2a4e322d624134ae
akrawitz <akrawitz@uvic.ca>
2019-07-12 14:23:54 -0700

    Add class names to code "labels" in text. Allow for nifty formatting possibilities.

---
+++ packages/detectable/remark-codeclass.js
@@ -0,0 +1,23 @@
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */ // devDeps okay
+/* eslint-disable new-cap */ // devDeps start with lowercase letter!
+/* eslint-disable no-console */ // writing to console is okay for gulpfile!
+
+// devDependencies
+const unistUtilVisit = require('unist-util-visit');
+
+function remarkCodeclass(options = {prefix: ''}) {
+  return (tree /* , file */) => {
+    // Walk markdown parsed into MDAST finding and processing all citations
+    unistUtilVisit(tree, 'inlineCode', (node /* , i, parent */) => {
+      // console.log(node);
+
+      const codeClass = options.prefix + node.value.split(' ')[0];
+
+      node.data = {hProperties: {className: [codeClass]}};
+
+      // console.log(node);
+    });
+  };
+}
+
+module.exports = remarkCodeclass;
