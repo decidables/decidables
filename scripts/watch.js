5fd344e581dce57072ba0b00c13334391dd2308e
Adam Krawitz <akrawitz@uvic.ca>
2020-04-24 22:22:17 -0700

    Fix gulp watch scripts. Can now watch specified local libraries.

--- scripts/watch.js
+++ scripts/watch.js
@@ -2,18 +2,26 @@
 // devDependencies
 const gulp = require('gulp');
 
+// Local dependencies
+const compiles = require('./compile');
+
 // Tasks
+function watchLibrariesTask(libraries) {
+  gulp.watch(libraries.map((library) => { return `../${library}/src/**/*.js`; }), {ignoreInitial: true}, compiles.compileScripts);
+}
+exports.watchLibrariesTask = watchLibrariesTask;
+
 function watchMarkdown() {
-  gulp.watch('src/**/*.{md,ejs}', {ignoreInitial: false}, gulp.series('compile:markdown'));
+  gulp.watch('src/**/*.{md,ejs}', {ignoreInitial: false}, compiles.compileMarkdown);
 }
 exports.watchMarkdown = watchMarkdown;
 
 function watchScripts() {
-  gulp.watch(['src/**/*.js'], {ignoreInitial: false}, gulp.series('compile:scripts'));
+  gulp.watch(['src/**/*.js'], {ignoreInitial: false}, compiles.compileScripts);
 }
 exports.watchScripts = watchScripts;
 
 function watchStyles() {
-  gulp.watch('src/**/*.scss', {ignoreInitial: false}, gulp.series('compile:styles'));
+  gulp.watch('src/**/*.scss', {ignoreInitial: false}, compiles.compileStyles);
 }
 exports.watchStyles = watchStyles;
