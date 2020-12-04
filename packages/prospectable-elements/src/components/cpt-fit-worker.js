7d43eea212d43656e92bbc752c4549e5513fb21e
Adam Krawitz <akrawitz@uvic.ca>
2020-12-04 15:40:30 -0800

    Add new component that does a Bayesian fit to CPT using a WebWorker!

---
+++ packages/prospectable-elements/src/components/cpt-fit-worker.js
@@ -0,0 +1,100 @@
+import * as BayesDistributions from 'bayes.js/distributions';
+import * as BayesMcmc from 'bayes.js/mcmc';
+import * as d3 from 'd3';
+
+import CPTMath from '@decidable/prospectable-math';
+
+export default function CPTFitWorker(self) {
+  self.onmessage = (event) => {
+    const params = {
+      alpha: {type: 'real', lower: 0, upper: 1},
+      lambda: {type: 'real', lower: 0, upper: 10},
+      gamma: {type: 'real', lower: 0, upper: 1},
+      luce: {type: 'real', lower: 0, upper: 100},
+    };
+
+    const logPost = (state, data) => {
+      let lp = 0;
+
+      // Priors
+      const alphaMean = 0.5;
+      const alphaSampleSize = 2.5;
+      lp += BayesDistributions.beta(
+        state.alpha,
+        alphaMean * alphaSampleSize,
+        (1 - alphaMean) * alphaSampleSize,
+      );
+      // lp += BayesDistributions.unif(state.alpha, 0, 1);
+
+      const lambdaMean = 2;
+      const lambdaShape = 3;
+      lp += BayesDistributions.gamma(
+        state.lambda,
+        lambdaShape,
+        lambdaShape / lambdaMean,
+      );
+      // lp += BayesDistributions.unif(state.lambda, 0, 10);
+
+      const gammaMean = 0.5;
+      const gammaSampleSize = 2.5;
+      lp += BayesDistributions.beta(
+        state.gamma,
+        gammaMean * gammaSampleSize,
+        (1 - gammaMean) * gammaSampleSize,
+      );
+      // lp += BayesDistributions.unif(state.gamma, 0, 1);
+
+      const luceMean = 2;
+      const luceShape = 3;
+      lp += BayesDistributions.gamma(
+        state.luce,
+        luceShape,
+        luceShape / luceMean,
+      );
+      // lp += BayesDistributions.unif(state.luce, 0, 100);
+
+      // Likelihood
+      data.forEach((choice) => {
+        // Values
+        const vl = CPTMath.xal2v(choice.xl, state.alpha, state.lambda);
+        const vw = CPTMath.xal2v(choice.xw, state.alpha, state.lambda);
+        const vs = CPTMath.xal2v(choice.xs, state.alpha, state.lambda);
+
+        // Probabilities
+        const ww = CPTMath.pg2w(choice.pw, state.gamma);
+        const wl = 1 - ww;
+
+        // Utilities
+        const ug = wl * vl + ww * vw;
+        const us = vs;
+
+        // Choice of gamble or sure is sampled from a Bernoulli distribution
+        // Luce choice rule is used to compute probability of gambling! (0 = sure, 1 = gamble)
+        const binval = 1 / (1 + Math.exp(state.luce * (us - ug)));
+
+        // Actual response
+        const response = (choice.response === 'sure') ? 0 : 1;
+
+        lp += BayesDistributions.bern(response, binval);
+      });
+
+      return lp;
+    };
+
+    // Initializing the sampler
+    const sampler = new BayesMcmc.AmwgSampler(params, logPost, event.data);
+    // Burning some samples to the MCMC gods and sampling 5000 draws
+    sampler.burn(1000);
+    const samples = sampler.sample(5000);
+
+    // Extract summary stats
+    const results = {
+      alpha: d3.median(samples.alpha),
+      lambda: d3.median(samples.lambda),
+      gamma: d3.median(samples.gamma),
+      luce: d3.median(samples.luce),
+    };
+
+    self.postMessage({results: results, samples: samples});
+  };
+}
---
+++ packages/prospectable-elements/src/components/cpt-fit.js
@@ -0,0 +1,196 @@
+
+import {html, css} from 'lit-element';
+import * as Plotly from 'plotly.js/lib/core';
+import * as PlotlyHistogram from 'plotly.js/lib/histogram';
+import Webworkify from 'webworkify';
+
+import CPTElement from '../cpt-element';
+import CPTFitWorker from './cpt-fit-worker';
+import plotlyStyle from './plotly-style';
+
+// Load in the needed trace types
+Plotly.register([PlotlyHistogram]);
+
+/*
+  CPTFit element
+  <cpt-fit>
+
+  Attributes:
+    interactive: true/false
+
+*/
+export default class CPTFit extends CPTElement {
+  static get properties() {
+    return {
+    };
+  }
+
+  constructor() {
+    super();
+
+    this.a = 1;
+    this.l = 1;
+    this.g = 1;
+
+    this.choices = [];
+    this.samples = null;
+
+    this.working = false;
+    this.queued = false;
+    this.worker = Webworkify(CPTFitWorker);
+
+    this.worker.onmessage = (event) => {
+      this.working = false;
+      this.samples = event.data.samples;
+      this.a = event.data.results.alpha;
+      this.l = event.data.results.lambda;
+      this.g = event.data.results.gamma;
+      this.requestUpdate();
+
+      this.dispatchEvent(new CustomEvent('cpt-fit-update', {
+        detail: {
+          a: this.a,
+          l: this.l,
+          g: this.g,
+        },
+        bubbles: true,
+      }));
+
+      if (this.queued) {
+        this.fit();
+      }
+    };
+
+    this.fit();
+  }
+
+  fit() {
+    if (!this.working) {
+      this.worker.postMessage(this.choices);
+      this.working = true;
+      this.queued = false;
+    } else {
+      this.queued = true;
+    }
+  }
+
+  clear() {
+    this.choices = [];
+
+    this.fit();
+  }
+
+  get(name = 'default') {
+    const choice = this.choices.find((item) => {
+      return (item.name === name);
+    });
+
+    return (choice === undefined) ? null : choice;
+  }
+
+  set(xl, xw, pw, xs, response, name = '', label = '') {
+    const choice = this.choices.find((item) => {
+      return (item.name === name);
+    });
+    if (choice === undefined) {
+      this.choices.push({
+        xl: xl,
+        xw: xw,
+        pw: pw,
+        xs: xs,
+        response: response,
+        name: name,
+        label: label,
+      });
+    } else {
+      choice.xl = xl;
+      choice.xw = xw;
+      choice.pw = pw;
+      choice.xs = xs;
+      choice.response = response;
+      choice.label = label;
+    }
+
+    this.fit();
+  }
+
+  static get styles() {
+    return [
+      super.styles,
+      plotlyStyle,
+      css`
+        /* :host {
+          display: inline-block;
+        } */
+
+        .trace,
+        .hist {
+          display: inline-block;
+
+          width: 20rem;
+          height: 15rem;
+        }
+      `,
+    ];
+  }
+
+  render() {
+    return html`
+      <div>
+        <div>After ${this.choices.length} trials:</div>
+        <div>Current:
+          <var class="math-var a">α</var> = ${this.a.toFixed(2)},
+          <var class="math-var l">λ</var> = ${this.l.toFixed(2)},
+          <var class="math-var g">γ</var> = ${this.g.toFixed(2)}
+        </div>
+        <div class="param">
+          <div class="trace alpha"></div>
+          <div class="hist alpha"></div>
+        </div>
+        <div class="param">
+          <div class="trace lambda"></div>
+          <div class="hist lambda"></div>
+        </div>
+        <div class="param">
+          <div class="trace gamma"></div>
+          <div class="hist gamma"></div>
+        </div>
+        <div class="param">
+          <div class="trace luce"></div>
+          <div class="hist luce"></div>
+        </div>
+      </div>
+    `;
+  }
+
+  plotParam(param) {
+    const plotMargins = {
+      l: 40, r: 10, b: 40, t: 40, pad: 4,
+    };
+
+    Plotly.react(
+      this.shadowRoot.querySelector(`.trace.${param}`),
+      [{y: this.samples[param]}],
+      {margin: plotMargins, title: `Traceplot of ${param}`},
+    );
+
+    Plotly.react(
+      this.shadowRoot.querySelector(`.hist.${param}`),
+      [{x: this.samples[param], type: 'histogram'}],
+      {margin: plotMargins, title: `Posterior of ${param}`},
+    );
+  }
+
+  update(changedProperties) {
+    super.update(changedProperties);
+
+    if (this.samples !== null) {
+      this.plotParam('alpha');
+      this.plotParam('lambda');
+      this.plotParam('gamma');
+      this.plotParam('luce');
+    }
+  }
+}
+
+customElements.define('cpt-fit', CPTFit);
