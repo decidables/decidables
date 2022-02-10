2d188811077c98ae83e8547e2a89fdf608079903
Adam Krawitz <akrawitz@uvic.ca>
2022-02-10 10:40:47 -0800

    decidables: Add version and date to footer.

--- scripts/utility.js
+++ scripts/utility.js
@@ -17,3 +17,11 @@ export function getPackageName() {
 export function getPackageNameCamelCase() {
   return camelcase(process.env.npm_package_name.split('/')[1]);
 }
+
+export function getPackageVersion() {
+  return process.env.npm_package_version;
+}
+
+export function getCurrentDate() {
+  return (new Date).toLocaleDateString('en-CA', {dateStyle: 'medium'});
+}
