1d13da3e2e02a2c0257b4754efe2762306564da6
Adam Krawitz <akrawitz@uvic.ca>
2022-02-28 13:15:51 -0800

    build(decidables-elements): add testing framework
    Initial installation of a testing framework
    Using @web/test-runner + @open-wc/testing
    Should be used by each  library before release!

---
+++ libraries/decidables-elements/test/button.test.js
@@ -0,0 +1,29 @@
+import {expect, fixture, html} from '@open-wc/testing';
+
+import '../src/button';
+
+describe('decidables-button', () => {
+  it('has a shadowDom', async () => {
+    const el = await fixture(html` <decidables-button>Push</decidables-button> `);
+
+    expect(el).shadowDom.to.equal(`
+      <button>
+        <slot></slot>
+      </button>
+    `);
+  });
+
+  it('has a lightDom', async () => {
+    const el = await fixture(html` <decidables-button>Push</decidables-button> `);
+
+    expect(el).lightDom.to.equal(`
+      Push
+    `);
+  });
+
+  it('can be disabled', async () => {
+    const el = await fixture(html` <decidables-button disabled>Push</decidables-button> `);
+
+    expect(el.shadowRoot).to.have.descendant('button').that.has.attribute('disabled');
+  });
+});
