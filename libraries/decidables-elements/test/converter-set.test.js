7da8d66c17d37c3a1610785e4510588a5c986848
Adam Krawitz <akrawitz@uvic.ca>
2022-03-01 13:45:45 -0800

    test(decidables-elements): tests for every component!

---
+++ libraries/decidables-elements/test/converter-set.test.js
@@ -0,0 +1,15 @@
+import {expect} from '@open-wc/testing';
+
+import DecidablesConvertSet from '../src/converter-set';
+
+describe('decidables-converter-set', () => {
+  it('converts from attribute to property', () => {
+    expect(DecidablesConvertSet.fromAttribute('alpha beta gamma'))
+      .to.deep.equal(new Set(['alpha', 'beta', 'gamma']));
+  });
+
+  it('converts from property to attribute', () => {
+    expect(DecidablesConvertSet.toAttribute(new Set(['alpha', 'beta', 'gamma'])))
+      .to.equal('alpha beta gamma');
+  });
+});
