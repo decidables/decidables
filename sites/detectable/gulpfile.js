4af428c05d8c3ccbcfddc7a5b84d9cddf5b46ca3
Adam Krawitz <akrawitz@uvic.ca>
2022-03-28 12:06:53 -0700

    feat(detectable): add favicon
    First take on a detectable favicon

--- sites/detectable/gulpfile.js
+++ sites/detectable/gulpfile.js
@@ -30,6 +30,14 @@ export const lint = gulp.parallel(
   lints.lintStyles,
 );
 
+export const compileFavicons = compiles.compileFaviconsTask({
+  appName: 'd′etectable',
+  appShortName: 'd′etectable',
+  appDescription: 'An Explorable Explanation of Signal Detection Theory',
+  background: '#e6e6e6',
+  theme_color: '#e6e6e6',
+});
+
 export const compileFonts = compiles.compileFontsTask([
   'source-code-pro/WOFF{,2}/OTF/SourceCodePro-{,Regular,Semibold,Bold}{,It}.otf.woff{,2}',
   'source-sans/WOFF{,2}/OTF/SourceSans3-{,Regular,Semibold,Bold}{,It}.otf.woff{,2}',
@@ -42,6 +50,7 @@ export const compileFonts = compiles.compileFontsTask([
 export const compile = gulp.series(
   cleans.cleanLocal,
   gulp.parallel(
+    compileFavicons,
     compileFonts,
     compiles.compileMarkdown,
     compiles.compileScripts,
@@ -65,6 +74,7 @@ export const watch = gulp.parallel(
 export const build = gulp.series(
   cleans.cleanDist,
   gulp.parallel(
+    builds.buildFavicons,
     builds.buildFonts,
     builds.buildMarkup,
     builds.buildScripts,
