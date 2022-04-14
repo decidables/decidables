ed202eee53cf1704c14bb61e6698ed4cedb9c3d6
Adam Krawitz <akrawitz@uvic.ca>
2022-04-14 15:06:16 -0700

    refactor(prospectable-elements): rename decision-outcome to risky-outcome

--- libraries/prospectable-elements/src/components/index.js
+++ libraries/prospectable-elements/src/components/index.js
@@ -7,7 +7,7 @@ export {default as CPTValue} from './cpt-value';
 export {default as DecisionChoice} from './decision-choice';
 export {default as ProspectableControl} from './prospectable-control';
 export {default as DecisionOption} from './decision-option';
-export {default as DecisionOutcome} from './decision-outcome';
+export {default as RiskyOutcome} from './risky-outcome';
 export {default as DecisionResponse} from './decision-response';
 export {default as DecisionSpace} from './decision-space';
 export {default as DecisionTask} from './decision-task';
