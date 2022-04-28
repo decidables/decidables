c1be96d2e2910734ec9df8f2aa506ba93e6511ae
Adam Krawitz <akrawitz@uvic.ca>
2022-04-27 20:49:53 -0700

    build(all): update dependencies

--- scripts/compile.js
+++ scripts/compile.js
@@ -169,7 +169,7 @@ const pluginBabel = rollupPluginBabel.babel({
   presets: [['@babel/preset-env', {
     bugfixes: true,
     useBuiltIns: 'entry',
-    corejs: '3.21',
+    corejs: '3.22',
   }]],
   babelHelpers: 'bundled',
 });
