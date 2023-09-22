a9b0da0a8af35e7fe4a4c5733969db559d137156
Adam Krawitz <akrawitz@uvic.ca>
2023-09-22 13:40:18 -0700

    feat(discountable-elements): initial take on ITC task

--- libraries/discountable-elements/src/components/index.js
+++ libraries/discountable-elements/src/components/index.js
@@ -1,8 +1,6 @@
 
 // export {default as HTDFit} from './htd-fit';
 export {default as HTDCurves} from './htd-curves';
-// export {default as DiscountableControl} from './discountable-control';
-// export {default as DiscountableResponse} from './discountable-response';
-// export {default as ITCChoice} from './itc-choice';
-// export {default as ITCOption} from './itc-option';
-// export {default as ITCTask} from './itc-task';
+export {default as ITCChoice} from './itc-choice';
+export {default as ITCOption} from './itc-option';
+export {default as ITCTask} from './itc-task';
