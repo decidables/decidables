8bc0875220b75a912e62ea25233e55d33f9150aa
Adam Krawitz <akrawitz@uvic.ca>
2024-06-20 21:43:24 -0700

    feat(accumulable-math): first commit for accumulable-math

---
+++ libraries/accumulable-math/src/index.js
@@ -0,0 +1,5 @@
+
+// Internal dependencies
+import DDMMath from './ddm-math';
+
+export default DDMMath;
---
+++ libraries/accumulable-math/test/ddm-math.test.js
@@ -0,0 +1,18 @@
+// import {expect} from '../../../scripts/test-utility';
+
+// import DDMMath from '../src/ddm-math';
+
+describe('ddm-math', () => {
+  // it('calculates value from amount, delay, and discounting factor', () => {
+  //   expect(DDMMath.adk2v(100, 19, 1)).to.equal(5);
+  //   expect(DDMMath.adk2v(50, 0, 0.5)).to.equal(50);
+  //   expect(DDMMath.adk2v(0, 40, 0.25)).to.equal(0);
+  //   expect(DDMMath.adk2v(20, 10, 0)).to.equal(20);
+  // });
+
+  // it('calculates discounting factor from amount, delay, and value', () => {
+  //   expect(DDMMath.adv2k(100, 19, 5)).to.equal(1);
+  //   expect(DDMMath.adv2k(50, 40, 50)).to.equal(0);
+  //   expect(DDMMath.adv2k(20, 5, 10)).to.equal(0.2);
+  // });
+});
